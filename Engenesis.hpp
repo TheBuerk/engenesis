cafdaa77168990c9fda38777d3452ad5a6aefa48
loic.boutter@gmail.com <loic.boutter@gmail.com@b50087bb-dc57-3a56-413b-bd79cfbcf086>
2012-09-11 14:30:28 +0000

    Structure directory

---
+++ Engenesis.hpp
@@ -0,0 +1,21 @@
+#ifndef ENGENESIS_HPP_INCLUDED
+#define ENGENESIS_HPP_INCLUDED
+
+#include "logger/logger.h"
+#include "logger/file_logger.h"
+#include "logger/logger_macros.h"
+
+#include "component.h"
+#include "content_manager.h"
+#include "entity.h"
+#include "exception.h"
+#include "game.h"
+#include "game_state.h"
+#include "non_copyable.h"
+#include "random.h"
+#include "resource.h"
+#include "singleton.h"
+#include "stop_watch.h"
+#include "system.h"
+
+#endif // ENGENESIS_HPP_INCLUDED
